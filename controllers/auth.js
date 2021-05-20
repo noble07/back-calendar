f927f1c692b14d62e7af1cca1e3a28ddb22ec95c
Juan Camilo Munoz <jcmunoz@smdigital.com.co>
2021-05-20 14:00:26 -0500

    Se agrega conexión a MongoDB y creación de usuario en la DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,38 @@
 const {response} = require('express') // Solo es para habilitar el IntelliSense
+const User = require('../models/User')
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async(req, res = response) => {
 
-  const {name, email, password} = req.body
+  const {email, password} = req.body
 
-  res.status(201).json({
-    ok: true,
-    msg: 'Registro',
-    name,
-    email,
-    password
-  })
+  try {
+
+    let user = await User.findOne({ email })
+
+    if(user){
+      return res.status(400).json({
+        ok: false,
+        msg: 'Un usuario existe con ese corre'
+      })
+    }
+
+    user = new User(req.body)
+  
+    await user.save()
+  
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    })
+  }
+
 }
 
 const loginUsuario = (req, res = response) => {
